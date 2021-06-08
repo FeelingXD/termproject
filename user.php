f112e00429821e951c1820a5b8ab31915f98c0bd
jimin <45274316+FeelingXD@users.noreply.github.com>
2021-06-08 22:07:20 +0900

    6-8 10pm commit

--- user.php
+++ user.php
@@ -1,3 +1,6 @@
+<?php 
+    include_once "db.php";
+    ?>
 <style>
     #frame1 {
         float: left;
@@ -5,7 +8,12 @@
         width: 50%;
         overflow: hidden;
     }
-
+    #frame1_subject{
+        background-color: #3c3c3c;
+        color: white;
+        box-sizing: border-box;
+        border: lightslategray solid 2px;
+    }
     #frame2 {
         float: left;
         height: 100%;
@@ -13,19 +21,42 @@
         overflow: hidden;
         background-color: lightblue;
     }
+    #frame2_subject{
+        background-color: #3c3c3c;
+        color: white;
+        box-sizing: border-box;
+        border: lightslategray solid 2px;
+    }
 </style>
 <div id="frame1">
-    <div>
+    <div id="frame1_subject">
         구역관리
     </div>
     <?php
-
+        $frame1_sql="";
+        // sql 작성
     ?>
-
+      <table>
+        <thead>
+            <th></th>
+            <th></th>
+        </thead>    
+    </table> 
 </div>
 <div id="frame2">
-    <div>
+    <div id="frame2_subject">
         내가쓴글 
     </div>
+    <?php
+    $frame2_sql="";
+    // sql 작성
+    ?>
+    
+    <table>
+        <thead>
+            <th></th>
+            <th></th>
+        </thead>    
 
+    </table>
 </div>
