ef848558062e70b18c77ed072a4c6f7374671c4a
jimin <45274316+FeelingXD@users.noreply.github.com>
2021-06-09 22:51:04 +0900

    6-9 commit

---
+++ test/view_notice.php
@@ -0,0 +1,63 @@
+<?php
+$id = $_GET['id'];
+include "db.php";
+?>
+
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/6c03ff6e85.js" crossorigin="anonymous"></script>
+    <!--폰트 어썸 !-->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script><!-- 제이쿼리 -->
+    <script type="text/javascript" src="./js/common.js"></script> <!-- 개인 스크립트 -->
+    <link rel="stylesheet" href="./css/style.css"> <!--  스타일 시트 주소 -->
+    <title>관리페이지</title>
+</head>
+
+<body>
+    <header>
+        <?php include "header.php"; ?>
+    </header>
+    <section>
+        <div id="status_content">
+            <div id="status_menu">
+                <div id="status_login">
+                    <b><?php echo $_SESSION['userid'] ?></b>님 환영합니다<br>
+                    등급 : <?php echo $_SESSION['u_rank'] ?>
+                    <div id="status_logout">
+                        <form action="logout.php" method="POST">
+                            <input type="submit" value="로그아웃">
+                        </form>
+                        <?php
+                        if ($_SESSION['u_rank'] == 'admin')
+                            echo "<button onclick=\"set_user()\" value=\"회원관리\">회원관리</button>"
+                        ?>
+
+                    </div>
+                </div>
+                <ul>
+                    <li id="home"><span style="color:gray"><i class="fas fa-home"></i></span></li>
+                    <li id="gear"><span style="color:gray"><i class="fas fa-cog"></i></span></li>
+                    <li id="user"><span style="color:gray"><i class="fas fa-users"></i></span></li>
+                </ul>
+            </div>
+            <div id="status_target">
+                    <?php
+                        // sql 작성
+                    ?>
+                    <div id="target_title">제목 :  sql 들어갈것</div>
+                    <div id="target_title">작성자 : sql 들어갈것 </div>
+                    <div id="target_title">내용 :  sql 들어갈것 </div>
+
+            </div>
+    </section>
+    <footer>
+        <?php include "footer.php"; ?>
+    </footer>
+</body>
+
+</html>
